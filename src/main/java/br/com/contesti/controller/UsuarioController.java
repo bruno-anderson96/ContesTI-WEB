c7840b1ac87047f559c12eeb94ff8b5f47bb8cff
caveiraman <brunoanderson.macedorosendo@gmail.com>
2016-11-05 15:14:30 -0300

    Validando usuario

--- src/main/java/br/com/contesti/controller/UsuarioController.java
+++ src/main/java/br/com/contesti/controller/UsuarioController.java
@@ -4,16 +4,17 @@ import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import br.com.contesti.entidades.Alternativa;
-import br.com.contesti.entidades.Questao;
 import br.com.contesti.entidades.Usuario;
 import br.com.contesti.repository.UsuarioRepository;
 
@@ -28,12 +29,13 @@ public class UsuarioController {
     @ResponseBody  
     public String create(@RequestParam String nome_usuario,@RequestParam String login,
     					 @RequestParam String senha,@RequestParam String email,
-    					 Boolean permissao,
+    					 Boolean ativo,
     					 @RequestParam String confirmar_senha,
-    					 Model model) throws NoSuchAlgorithmException, UnsupportedEncodingException{	
+    					 Model model,
+    					 @Valid Usuario usuario, BindingResult result) throws NoSuchAlgorithmException, UnsupportedEncodingException{	
 		
     		if(senha.equals(confirmar_senha)){    	
-    			
+    			ativo = true;
     			MessageDigest algorithm = MessageDigest.getInstance("SHA-256");
     			byte messageDigest[] = algorithm.digest(senha.getBytes("UTF-8"));
     			StringBuilder hexString = new StringBuilder();
@@ -41,7 +43,7 @@ public class UsuarioController {
     				hexString.append(String.format("%02X",  0xFF & b));
     			}
     			senha = hexString.toString();
-    			usuarioRepository.save(new Usuario(nome_usuario,login,senha,email,false));    	       	
+    			usuarioRepository.save(usuario);    	       	
     	    	return "Sucesso!!!";
     		}else{
     			return "Senha diferente";
