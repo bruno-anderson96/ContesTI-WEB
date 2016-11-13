0c84bb9353d92160bde16a9723dbc81e96d564e9
caveiraman <brunoanderson.macedorosendo@gmail.com>
2016-11-13 17:03:30 -0300

    Novas telas principais adicionadas

--- src/main/java/br/com/contesti/controller/QuestaoController.java
+++ src/main/java/br/com/contesti/controller/QuestaoController.java
@@ -45,7 +45,7 @@ public class QuestaoController {
 
 	@RequestMapping(value = "/criarQuestaoUsuario", method = RequestMethod.POST)
 	@ResponseBody
-	public ModelAndView create(@RequestParam String pergunta, @RequestParam String isCorreta, Boolean correta1,
+	public ModelAndView create(@RequestParam String pergunta,@RequestParam String isCorreta, Boolean correta1,
 			Boolean correta2, Boolean correta3, Boolean correta4, Boolean correta5, @RequestParam String desc1,
 			@RequestParam String desc2, @RequestParam String desc3, @RequestParam String desc4,
 			@RequestParam String desc5, @RequestParam Disciplina disciplina, @RequestParam Assunto assunto,
@@ -80,34 +80,42 @@ public class QuestaoController {
 				} else {
 					correta5 = false;
 				}
+				
+			
 
-				disciplina = disciplinaRepository.findOne(disciplina.getId());
-				assunto = assuntoRepository.findOne(assunto.getId());
+			disciplina = disciplinaRepository.findOne(disciplina.getId());
+			assunto = assuntoRepository.findOne(assunto.getId());
 
-				questao = new Questao(pergunta, questao, assunto);
-				questaoRepository.save(questao);
+			questao = new Questao(pergunta, questao, assunto);
+			questaoRepository.save(questao);
 
-				Alternativa a1 = new Alternativa(desc1, correta1, questao);
-				Alternativa a2 = new Alternativa(desc2, correta2, questao);
-				Alternativa a3 = new Alternativa(desc3, correta3, questao);
-				Alternativa a4 = new Alternativa(desc4, correta4, questao);
-				Alternativa a5 = new Alternativa(desc5, correta5, questao);
-				alternativaRepository.save(a1);
-				alternativaRepository.save(a2);
-				alternativaRepository.save(a3);
-				alternativaRepository.save(a4);
-				alternativaRepository.save(a5);
+			Alternativa a1 = new Alternativa(desc1, correta1, questao);
+			Alternativa a2 = new Alternativa(desc2, correta2, questao);
+			Alternativa a3 = new Alternativa(desc3, correta3, questao);
+			Alternativa a4 = new Alternativa(desc4, correta4, questao);
+			Alternativa a5 = new Alternativa(desc5, correta5, questao);
+			alternativaRepository.save(a1);
+			alternativaRepository.save(a2);
+			alternativaRepository.save(a3);
+			alternativaRepository.save(a4);
+			alternativaRepository.save(a5);
 
-				attributes.addFlashAttribute("mensagem", "Questão cadastrada com sucesso!");
-				return mv;
-			} else {
-				attributes.addFlashAttribute("erro", "Preencha todas as descrições");
-				return mv;
-			}
+			attributes.addFlashAttribute("mensagem", "Questão cadastrada com sucesso!");
+			return mv;
 		} else {
-			attributes.addFlashAttribute("erro", "Preencha o campo pergunta");
+			
+			attributes.addFlashAttribute("erro", "Preencha todas as descrições");
+			
 			return mv;
 		}
+	}else
+
+	{
+		
+		attributes.addFlashAttribute("erro", "Preencha o campo pergunta");
+		
+		return mv;
+	}
 
 	}
 
--- src/main/java/br/com/contesti/repository/AssuntoRepository.java
+++ src/main/java/br/com/contesti/repository/AssuntoRepository.java
@@ -3,7 +3,11 @@ package br.com.contesti.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.contesti.entidades.Assunto;
+import br.com.contesti.entidades.Disciplina;
 
 public interface AssuntoRepository  extends JpaRepository<Assunto, Long> {
 
+	Assunto findByDisciplina(Disciplina disciplina);
+	
+	
 }
